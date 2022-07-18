9355d9cf38e8a3a8c3a40f4919b2e75bc3e2368b
lilianAIglobal <109298838+lilianAIglobal@users.noreply.github.com>
2022-07-18 14:53:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ streamlit.text("🍞🥑 Avocado toast")
 streamlit.header("🍌🍓 Build your own fruit smoothie 🍉🍑")
 
 fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),["Avocado","Strawberries"])
-fruits_to_show = my_fruit_list.loc["fruits_selected"]
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
