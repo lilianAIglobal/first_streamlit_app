e138dd65a4c0777bda6fb4568e47f4bca2bf5928
lilianAIglobal <109298838+lilianAIglobal@users.noreply.github.com>
2022-07-18 14:51:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header('Breakfast Favorites')
@@ -12,6 +13,7 @@ streamlit.text("🍞🥑 Avocado toast")
 
 streamlit.header("🍌🍓 Build your own fruit smoothie 🍉🍑")
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.dataframe(my_fruit_list)
+fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),["Avocado","Strawberries"])
+fruits_to_show = my_fruit_list.loc["fruits_selected"]
+streamlit.dataframe(fruits_to_show)
 
