663981854e8fd85f2415c9acaf0579e681d8fed3
lilianAIglobal <109298838+lilianAIglobal@users.noreply.github.com>
2022-07-26 11:57:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # put json into a table
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
